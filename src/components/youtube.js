107e66efd533ba5041d08a711869fb6220b13b7a
Thirumalai G <146057845+Thirumalai-Tech-Developer@users.noreply.github.com>
2024-10-05 20:59:51 +0530

    complete youtube data fetched

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -4,10 +4,11 @@ const Youtube = () => {
     const [search, setSearch] = useState('');
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+    const [length, setLength] = useState(null);
 
     useEffect(() => {
         if (search) {
-            fetch(`http://localhost:5000/youtube?search=${search}`)
+            fetch(`http://localhost:5000/youtube?search=${search}&maxResults=${length}`)
                 .then((res) => {
                     if (!res.ok) {
                         throw new Error('Network response was not ok');
@@ -23,7 +24,7 @@ const Youtube = () => {
                     setData(null);
                 });
         }
-    }, [search]);
+    }, [search, length]);
 
     return (
         <>
@@ -33,15 +34,28 @@ const Youtube = () => {
                     placeholder='Search'
                     onChange={(e) => setSearch(e.target.value)}
                 />
+                <input
+                    type='number'
+                    placeholder='No limit'
+                    onChange={(e) => setLength(Number(e.target.value))} // Convert to number
+                />
                 {error && <p>Error: {error}</p>}
                 {data && (
-                    <ul>
+                    <div>
                         {data.map((item) => (
-                            <li key={item.key}>
-                                <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
-                            </li>
+                            <iframe 
+                                key={item.key} 
+                                width="560" 
+                                height="315" 
+                                src={item.embedUrl} 
+                                title="YouTube video player" 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                referrerPolicy="strict-origin-when-cross-origin" 
+                                allowFullScreen>
+                            </iframe>
                         ))}
-                    </ul>
+                    </div>
                 )}
             </div>
         </>
